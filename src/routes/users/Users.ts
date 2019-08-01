0ddb5dbf45c995fe8e524b64253aaeabd198bd5f
Antigha Henshaw <Ghost@localhost.localdomain>
2019-08-01 19:53:57 +0100

    added login route

--- src/routes/users/Users.ts
+++ src/routes/users/Users.ts
@@ -1,10 +1,11 @@
 import { logger } from '@shared';
 import { Request, Response, Router } from 'express';
-import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { BAD_REQUEST, CREATED, OK, INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import User from '../../models/User.model';
 import { IUser, IUserProfile } from 'src/interfaces/IUser';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import UserModel from '../../models/User.model';
 // Init router and path
 const router = Router();
 const path = '/users';
@@ -20,13 +21,12 @@ export const getUsersPath = '/create';
  * Create a new user and add to DB
  * Full Path: "GET /api/users/create"
  */
-router.get(getUsersPath, async (req: Request, res: Response) => {
+router.post(getUsersPath, async (req: Request, res: Response) => {
     try {
-        const foundUser = User.findOne({ email: req.body.email});
-
+        const foundUser = await User.findOne({ email: req.body.email });
         if (foundUser) {
-            res.json({
-                exsist: `Sorry, ${req.body.name} you have an account already, try logging in`
+            res.status(OK).json({
+                exsist: `Sorry, ${req.body.name} you have an account already, try logging in`,
             });
         } else {
             const userProfile: IUserProfile = {
@@ -41,7 +41,7 @@ router.get(getUsersPath, async (req: Request, res: Response) => {
                 email: req.body.email,
                 password: req.body.password,
                 phone_number: req.body.number,
-                user_profile: userProfile,
+                userProfile,
             });
             // Hash Password
             const saltRounds = 10;
@@ -50,72 +50,61 @@ router.get(getUsersPath, async (req: Request, res: Response) => {
             const saved = await user.save();
 
             // jwt
-            const payload = { userID: user._id};
+            const payload = { userID: user._id };
             const secret = process.env.JWT_SECRET as string;
             const token = jwt.sign(payload, secret);
 
             return res.status(CREATED).json({
                 userID: user._id,
                 jwt: token,
-                success: `${req.body.name} your account has been created, Welcome`,
+                // tslint:disable-next-line: max-line-length
+                success: `Your account has been created ${req.body.name.split(' ')[0].charAt(0).toUpperCase()}  Welcome`,
             });
         }
 
     } catch (error) {
+        res.status(INTERNAL_SERVER_ERROR).json({ err: 'Oops, an error occurred' });
         logger.error(error, error.message);
     }
-    res.send('done').status(200);
 });
 
 /******************************************************************************
- *                                Add One
+ *                                Login
  ******************************************************************************/
 
-// Constants
-export const addUserPath = '/add';
-export const userMissingErr = 'User property was not present for adding user route.';
+export const addUserPath = '/login';
 
-/**
- * Add one user.
- * Full Path: "POST /api/users/add"
- */
-router.post(addUserPath, async (req: Request, res: Response) => {
+export const errorMessage = 'Oops sorry, error logging you in';
+
+router.post('/login', async (req: Request, res: Response) => {
+    try {
+        const user = await UserModel.findOne({ _id: req.body._id });
+
+        if (user) {
+            const userPassword = user.password;
+            const requestPassword = req.body.password;
+            const samePassword = await bcrypt.compare(requestPassword, userPassword);
+            if (samePassword) {
+                if (req.session!.view) {
+                    req.session!.view++;
+                } else {
+                    req.session!.view = 0;
+                }
+                const payload = { userID: user._id };
+                const secret = process.env.JWT_SECRET as string;
+                const token = jwt.sign(payload, secret);
+                req.session!.userID = user._id;
+                // Split full name and capitalize the first name
+                req.session!.name = user.name.split(' ')[0].charAt(0).toUpperCase();
+                res.status(OK).json({
+                    token,
+                    userID: user._id,
+                    success: `Welcome back ${req.body.name.split(' ')[0].charAt(0).toUpperCase()}`,
+                });
+            }
+        }
+    } catch (error) {
+
+    }
 
 });
-
-/******************************************************************************
- *                                      Update
- ******************************************************************************/
-
-// Constants
-export const updateUserPath = '/update';
-export const userUpdateMissingErr = 'User property was not present for updating user route.';
-
-/**
- * Update one user.
- * Full Path: "PUT /api/users/update"
- */
-router.put(updateUserPath, async (req: Request, res: Response) => {
-
-});
-
-/******************************************************************************
- *                                      Delete
- ******************************************************************************/
-
-// Constants
-export const deleteUserPath = '/delete/:id';
-
-/**
- * Add one user.
- * Full Path: "DELETE /api/users/delete/:id"
- */
-router.delete(deleteUserPath, async (req: Request, res: Response) => {
-
-});
-
-/******************************************************************************
- *                                     Export
- ******************************************************************************/
-
-export default { router, path };
