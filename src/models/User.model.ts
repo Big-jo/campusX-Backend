1b9520a3dcd89f0f64df53d61fad916c9d8a878a
Antigha Henshaw <Ghost@localhost.localdomain>
2019-08-01 19:52:40 +0100

    change spelling of userTage to userTag

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -12,7 +12,7 @@ const UserProfileSchema: Schema = new Schema({
 
 const UserSchema: Schema = new Schema({
     name: {type: String, required: true},
-    userTage: {type: String, required: true},
+    userTag: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
     phoneNumber: {type: Number},
