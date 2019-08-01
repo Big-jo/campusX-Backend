d2f6f0bec2ed25d9034d364454582230802f1f33
Antigha Henshaw <Ghost@localhost.localdomain>
2019-08-01 19:48:50 +0100

    added sessions and mongoStore

--- src/Server.ts
+++ src/Server.ts
@@ -5,20 +5,38 @@ import logger from 'morgan';
 import path from 'path';
 import BaseRouter from './routes/Base';
 import mongoose from 'mongoose';
+import session = require('express-session');
+import connectMongo from 'connect-mongo';
+import uuid from 'uuid';
 
+// Create mongo store
+const mongoDBStore = connectMongo(session);
 // Setup MongoDB
 const URI = 'mongodb://localhost:27017/campusX';
 
-mongoose.connect(URI, {useNewUrlParser: true}).catch((err) => {
-    console.log(err.message, err);
-});
-// TODO: Add connection emitter for mongoose
+mongoose.connect(URI, {useNewUrlParser: true});
+
+// Connection Instance
+const Db = mongoose.connection;
+// Bind connection to error event
+
+// tslint:disable-next-line: no-console
+Db.on('error', console.error.bind(console, 'MongoDB connection error'));
+// tslint:disable-next-line: no-console
+Db.on('connected', console.log.bind(console, 'MongoDB connected'));
 
 // Init express
 const app = express();
 
 // Add middleware/settings/routes to express.
 app.use(logger('dev'));
+app.use(session({
+    genid: () => uuid(),
+    secret: process.env.SESSION_SECRET as string,
+    store:  new mongoDBStore({mongooseConnection: Db}),
+    resave: false,
+    saveUninitialized: false,
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
