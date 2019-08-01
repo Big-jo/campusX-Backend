89a8624e1e9db4d0879dedd34c80e008c81861d1
Antigha Henshaw <Ghost@localhost.localdomain>
2019-08-01 19:54:36 +0100

    Added some more test

--- src/routes/users/Users.test.ts
+++ src/routes/users/Users.test.ts
@@ -1,20 +1,62 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Server from '../../Server';
+import { CREATED } from 'http-status-codes';
+import { IUser } from 'src/interfaces/IUser';
+import UserModel from 'src/models/User.model';
 
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-// Test /Get route
+beforeEach(() => {
+    UserModel.collection.drop();
+});
 
-describe('/Get Users', () => {
-    it('it should get all users', (done) => {
+describe('/Create User', () => {
+    it('Create a user and return credentials', (done) => {
+        const userData = {
+            name: 'Joseph Henshaw',
+            email: 'furiousjoe16@gmail.com',
+            password: 'Mmedaraetuk16',
+            phone_number: '08180286155',
+            userTag: '@Bigjo',
+            gender: 'male',
+            user_profile: {
+                department: 'computer engineering',
+                gender: 'male',
+                level: 300,
+                university: 'Bells University Of Tecnology',
+            },
+        };
         chai.request(Server)
-            .get('/users')
+            .post('/api/users/create')
+            .send(userData)
             .end((err, res) => {
-                res.should.have.status(200);
+                if (err) { res.body.should.have.property('err'); }
+                res.body.should.have.property('userID');
+                res.body.should.have.property('jwt');
                 done();
             });
     });
+});
+
+describe('/Log In', () => {
+    it('Log a user in and return credentials ', (done) => {
+        const userData = {
+            email: 'furiousjoe16@gmail.com',
+            password: 'Mmedaraetuk16',
+        };
+        chai.request(Server)
+            .post('/api/users/create')
+            .send(userData)
+            .end((err, res) => {
+                if (err) { res.body.should.have.property('err'); }
+                res.body.should.have.property('userID');
+                res.body.should.have.property('success');
+                res.body.should.have.property('token');
+                done();
+            });
+    });
+
 });
