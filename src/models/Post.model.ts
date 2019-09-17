2a9c4c59cf43486728ed6e8ad68ad1b3743b8e74
Antigha Henshaw <Ghost@localhost.localdomain>
2019-09-17 13:37:31 +0100

    typed Post model as IPost

--- src/models/Post.model.ts
+++ src/models/Post.model.ts
@@ -1,10 +1,13 @@
-import mongoose, {Schema, Document} from 'mongoose';
+import mongoose, {Schema, Document, mongo} from 'mongoose';
+import { IPost } from 'src/interfaces/IPost';
 
 const PostSchema: Schema = new Schema({
-    author: {type: Schema.Types.ObjectId},
+    author: {type: Schema.Types.ObjectId, ref: 'User'},
     post: {type: String},
     likes: {type: Number},
     likedBy: [{type: Schema.Types.ObjectId}],
     createdAt: {type: String},
 });
+
 // TODO: Implement tags
+export default mongoose.model<IPost>('Post', PostSchema);
