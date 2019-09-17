20a82c01536d29b150ba2c804ac32d6a1a5948cc
Antigha Henshaw <Ghost@localhost.localdomain>
2019-09-17 13:31:05 +0100

    changed app to server

--- src/Start.ts
+++ src/Start.ts
@@ -1,8 +1,8 @@
-import app from '@server';
-import { logger } from '@shared';
+import server from './Server';
+import { logger } from './shared/Logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
+server.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
