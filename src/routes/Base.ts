894bdcf3a9da92c2febf2c9604e3f29ad94dcca0
Antigha Henshaw <Ghost@localhost.localdomain>
2019-09-17 13:39:32 +0100

    Added Post router

--- src/routes/Base.ts
+++ src/routes/Base.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import UserRouter from './users/Users';
-
+import PostRouter from './posts/Post';
 // Init router and path
 const router = Router();
-const path = '/api';
+const path = '/campusx/api/v1';
 
 // Add sub-routes
 router.use(UserRouter.path, UserRouter.router);
+router.use(PostRouter.path, PostRouter.router);
 
 // Export the base-router
 export default { router, path };
