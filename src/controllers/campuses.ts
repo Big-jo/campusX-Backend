ac9a5ce8ac850b8dea59762e7364326503a7fcee
Antigha Henshaw <Ghost@localhost.localdomain>
2019-10-28 17:55:54 +0100

    Only using core  campuses

--- src/controllers/campuses.ts
+++ src/controllers/campuses.ts
@@ -2,74 +2,75 @@ import CampusModel from '../models/Campus.model';
 import { Request, Response } from 'express';
 
 // tslint:disable-next-line: no-unused-expression
-const Universities = [{
-            Name: 'Adekunle Ajasin University',
-            Abbreviation: 'AAUA',
-      },
-      {
-            Name: 'Federal University Of Agriculture',
-            Abbreviation: 'FUNAAB',
-      },
-      {
-            Name: 'Abia State University',
-            Abbreviation: ' ABSU',
-      },
-      {
-            Name: 'Adekunle Ajasin University',
-            Abbreviation: 'AAUA',
-      },
-      {
-            Name: 'Joseph Ayo Babalola University',
-            Abbreviation: 'JABU',
-      },
-      {
-            Name: 'Redeemer/s University Nigeria',
-            Abbreviation: ' RUN',
-      },
+const Universities = [
+      // {
+      //       Name: 'Adekunle Ajasin University',
+      //       Abbreviation: 'AAUA',
+      // },
+      // {
+      //       Name: 'Federal University Of Agriculture',
+      //       Abbreviation: 'FUNAAB',
+      // },
+      // {
+      //       Name: 'Abia State University',
+      //       Abbreviation: ' ABSU',
+      // },
+      // {
+      //       Name: 'Adekunle Ajasin University',
+      //       Abbreviation: 'AAUA',
+      // },
+      // {
+      //       Name: 'Joseph Ayo Babalola University',
+      //       Abbreviation: 'JABU',
+      // },
+      // {
+      //       Name: 'Redeemer/s University Nigeria',
+      //       Abbreviation: ' RUN',
+      // },
       {
             Name: 'Afe Babalola University',
             Abbreviation: 'ABUAD',
       },
-      {
-            Name: 'Akwa Ibom State University',
-            Abbreviation: 'AKSU',
-      },
-      {
-            Name: 'American University Of Nigeria',
-            Abbreviation: 'AUN',
-      },
-      {
-            Name: 'Abubakar Tafawa Balewa University',
-            Abbreviation: 'ATBU',
-      },
-      {
-            Name: 'Adamawa State University',
-            Abbreviation: 'ADSU',
-      },
-      {
-            Name: 'Achievers University',
-            Abbreviation: 'AC',
-      },
-      {
-            Name: ' Al-Hikmah University',
-            Abbreviation: 'AHU',
-      },
-      {
-            Name: 'Ambrose Ali University',
-            Abbreviation: 'AAU',
-      },
-      {
-            Name: 'Anambra State University',
-            Abbreviation: 'ANSU',
-      },
-      {
-            Name: 'Ajayi Crowther University',
-            Abbreviation: 'ACU',
-      },
-      {
-            Name: 'Bayero University',
-            Abbreviation: 'BUK',
-      },
+      // {
+      //       Name: 'Akwa Ibom State University',
+      //       Abbreviation: 'AKSU',
+      // },
+      // {
+      //       Name: 'American University Of Nigeria',
+      //       Abbreviation: 'AUN',
+      // },
+      // {
+      //       Name: 'Abubakar Tafawa Balewa University',
+      //       Abbreviation: 'ATBU',
+      // },
+      // {
+      //       Name: 'Adamawa State University',
+      //       Abbreviation: 'ADSU',
+      // },
+      // {
+      //       Name: 'Achievers University',
+      //       Abbreviation: 'AC',
+      // },
+      // {
+      //       Name: ' Al-Hikmah University',
+      //       Abbreviation: 'AHU',
+      // },
+      // {
+      //       Name: 'Ambrose Ali University',
+      //       Abbreviation: 'AAU',
+      // },
+      // {
+      //       Name: 'Anambra State University',
+      //       Abbreviation: 'ANSU',
+      // },
+      // {
+      //       Name: 'Ajayi Crowther University',
+      //       Abbreviation: 'ACU',
+      // },
+      // {
+      //       Name: 'Bayero University',
+      //       Abbreviation: 'BUK',
+      // },
       {
             Name: 'Babcock University',
             Abbreviation: 'BU',
@@ -78,46 +79,46 @@ const Universities = [{
             Name: 'Bells University Of Technology',
             Abbreviation: 'BUT',
       },
-      {
-            Name: 'Benson Idahosa University Of Technology',
-            Abbreviation: 'BIU',
-      },
-      {
-            Name: 'Benue State University',
-            Abbreviation: 'BSU',
-      },
-      {
-            Name: 'ECWA Bingham University',
-            Abbreviation: 'BU',
-      },
-      {
-            Name: 'Bowen University ',
-            Abbreviation: 'BU',
-      },
-      {
-            Name: 'Bukar Abba Ibrahim University',
-            Abbreviation: 'YSU',
-      },
-      {
-            Name: 'Caleb University',
-            Abbreviation: 'CUI',
-      },
-      {
-            Name: 'Landmark University',
-            Abbreviation: 'LU',
-      },
-      {
-            Name: 'Nigerian Turkish Nile University',
-            Abbreviation: ' Abbv',
-      },
+      // {
+      //       Name: 'Benson Idahosa University Of Technology',
+      //       Abbreviation: 'BIU',
+      // },
+      // {
+      //       Name: 'Benue State University',
+      //       Abbreviation: 'BSU',
+      // },
+      // {
+      //       Name: 'ECWA Bingham University',
+      //       Abbreviation: 'BU',
+      // },
+      // {
+      //       Name: 'Bowen University ',
+      //       Abbreviation: 'BU',
+      // },
+      // {
+      //       Name: 'Bukar Abba Ibrahim University',
+      //       Abbreviation: 'YSU',
+      // },
+      // {
+      //       Name: 'Caleb University',
+      //       Abbreviation: 'CUI',
+      // },
+      // {
+      //       Name: 'Landmark University',
+      //       Abbreviation: 'LU',
+      // },
+      // {
+      //       Name: 'Nigerian Turkish Nile University',
+      //       Abbreviation: ' Abbv',
+      // },
       {
             Name: 'University Of Lagos',
             Abbreviation: ' UNILAG',
       },
-      {
-            Name: 'University Of Nigeria Nsukka',
-            Abbreviation: 'UNN',
-      },
+      // {
+      //       Name: 'University Of Nigeria Nsukka',
+      //       Abbreviation: 'UNN',
+      // },
       {
             Name: 'Convenant University',
             Abbreviation: 'CU',
