45d85225d30e84068569d7ad5a951d1e97943bad
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-08 21:07:25 +0100

    Initial commit

---
+++ src/models/Repost.model.ts
@@ -0,0 +1,17 @@
+
+import mongoose, { Schema, Document, mongo } from 'mongoose';
+import { IPostModel, IRepost } from 'src/interfaces/IPost';
+
+const RepostSchema: Schema = new Schema({
+    author: { type: String, ref: 'User' },
+    text: { type: String },
+    video: { type: String, default: null },
+    image: { type: String, default: null },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    trash: { type: Number, default: 0 },
+    createdAt: { type: String },
+
+});
+
+export default mongoose.model<IRepost>('Repost', RepostSchema);
