29b32f6b8999f6a5a8e61a16ea3022a448753ed5
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 19:52:15 +0100

    Add email and password fields

--- src/interfaces/IStore.ts
+++ src/interfaces/IStore.ts
@@ -1,10 +1,12 @@
 import { Document } from 'mongoose';
 
 export interface IStore {
+    email: string;
     name: string;
     owner: string;
     description: string;
     date_created: string;
+    password: string;
 }
 
 export interface IStoreModel extends IStore, Document {
