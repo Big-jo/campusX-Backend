6d1a25adc776c904f2d3cff717f1f36350c161a9
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 20:21:06 +0100

    Intial commit

---
+++ src/models/Transacation.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+import { ItemSchema } from './Item.model';
+import moment from 'moment';
+import { IStoreTransactionModel } from 'src/interfaces/IStoreTransaction';
+
+const StoreTransactionSchema = new Schema({
+    store: {type: Schema.Types.ObjectId, ref: 'Store'},
+    buyerEmail: {type: String, required: true},
+    buyerName: {type: String, required: true},
+    itemsPurchased: [ItemSchema],
+    Merchant: {type: Schema.Types.ObjectId, ref: 'Merchant', required: true},
+    date: {type: String, default: moment().format('lll')},
+    grandTotal: {type: String, required: true},
+});
+
+export default mongoose.model<IStoreTransactionModel>('StoreTransaction', StoreTransactionSchema);
