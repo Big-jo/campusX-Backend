30c4b99a9aca5e3052db94b8f410b98649dc1f4a
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 19:53:08 +0100

    Intial Commit

---
+++ src/models/CircleMember.model.ts
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from 'mongoose';
+import { ICircleMemberModel } from 'src/interfaces/ICircleMembers';
+
+const CircleMemberSchema = new Schema({
+    userID: {type: Schema.Types.ObjectId, ref: 'User'},
+});
+
+export default mongoose.model<ICircleMemberModel>('CircleMember', CircleMemberSchema);
