8f6eea9dc3433a659d425dbacf7999d7f0f24913
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 20:20:27 +0100

    Add price field

--- src/models/Item.model.ts
+++ src/models/Item.model.ts
@@ -5,12 +5,13 @@ import moment from 'moment';
 /**
  *  Merchant is the owner of a store
  */
-const ItemSchema: Schema = new Schema({
+export const ItemSchema: Schema = new Schema({
    name: {type: String, required: true},
    store: {type: Schema.Types.ObjectId, ref: 'Store', required: true},
    date_added: {type: String, required: true, default: moment().format('lll')},
    description: {type: String},
    sales: {type: Number, default: 0},
+   price: {type: String, required: true},
 });
 
 ItemSchema.index('store');
