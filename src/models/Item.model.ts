94387f308071aa7888a3aac7200ff074430240f6
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 19:53:30 +0100

    Add default time and index store field

--- src/models/Item.model.ts
+++ src/models/Item.model.ts
@@ -1,15 +1,18 @@
 import mongoose, {Schema} from 'mongoose';
 import {StoreSchema} from './Store.model';
 import {IItemModel} from '../interfaces/IItem';
+import moment from 'moment';
 /**
  *  Merchant is the owner of a store
  */
 const ItemSchema: Schema = new Schema({
    name: {type: String, required: true},
    store: {type: Schema.Types.ObjectId, ref: 'Store', required: true},
-   date_added: {type: String, required: true},
+   date_added: {type: String, required: true, default: moment().format('lll')},
    description: {type: String},
-   sales: {type: String},
+   sales: {type: Number, default: 0},
 });
+
+ItemSchema.index('store');
 
 export default mongoose.model<IItemModel>('Item', ItemSchema);
