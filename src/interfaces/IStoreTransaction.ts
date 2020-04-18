7a0750b08c4ee4be7772472ece808ba24cb2d608
Joseph Henshaw <furiousjoe16@gmail.com>
2020-04-18 21:32:03 +0100

    Change spelling of merchant

--- src/interfaces/IStoreTransaction.ts
+++ src/interfaces/IStoreTransaction.ts
@@ -6,7 +6,7 @@ export interface IStoreTransaction {
     buyerEmail: string;
     buyerName: string;
     itemsPurchased: IItem[];
-    Merchat?: string;
+    Merchant?: string;
     date?: string;
     grandTotal: number;
     currency: string;
