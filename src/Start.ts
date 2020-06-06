5f178700a90a3d566878ca2f3c295bdf5458c6a6
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-06 17:41:53 +0100

    import server using  destructring

--- src/Start.ts
+++ src/Start.ts
@@ -1,9 +1,10 @@
-import server from './Server';
+import {server} from './Server';
 import { logger } from './shared/Logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-server.listen(port, () => {
+
+server.listen(port,() => {
     logger.info('Express server started on port: ' + port);
 });
 
