dfb267fb8a7d99be8d03e43037e66b57ff2a736b
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-06 17:38:57 +0100

    removed unnecessary imports, called Newsfeed class and passed IO object to it

--- src/Server.ts
+++ src/Server.ts
@@ -1,33 +1,26 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import {
-	Request,
-	Response,
+    Request,
+    Response,
 } from 'express';
 import logger from 'morgan';
-import path from 'path';
 import mongoose, { Collection } from 'mongoose';
 import BaseRouter from './routes/Base';
-import session = require('express-session');
-import connectMongo from 'connect-mongo';
-import uuid from 'uuid';
 import cors from 'cors';
-import {
-	Server,
-	createServer,
-} from 'http';
-import {
-	Campus,
-} from './controllers/campuses';
-import { DBRef } from 'bson';
+import * as socketIO from 'socket.io';
+import * as http from 'http';
+import * as SocketIO from 'socket.io';
+import {Newsfeed} from './lib/newsfeeds';
+import {NOT_FOUND} from 'http-status-codes';
 
 // Setup MongoDB
 const URI = process.env.MONGO_URI as string;
 
 mongoose.connect(URI, {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 // Connection Instance
@@ -38,39 +31,47 @@ Db.on('error', console.error.bind(console, 'MongoDB connection error'));
 // tslint:disable-next-line: no-console
 Db.on('connected', console.log.bind(console, 'MongoDB connected'));
 
+
+
+
 // Init express
+
 const app = express();
+
+//  Setup socketIO
+const server = http.createServer(app);
+
+const io = socketIO.listen(server);
+// Handle Websockets
+// io.of('/get-newsfeed').on('connection', (socket: any) => {
+//     console.log(socket.id);
+// });
+
+
 
 app.use(cors());
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 // Add middleware/settings/routes to express.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
-	extended: true,
+    extended: true,
 }));
+app.use((req, res, next) => { res.locals.socketio = io; next(); });
 app.use(cookieParser());
 app.use(BaseRouter.path, BaseRouter.router);
-
-/**
- * Point express to the 'views' directory. If you're using a
- * single-page-application framework like react or angular
- * which has its own development server, you might want to
- * configure this to only serve the index file while in
- * production mode.
- */
-// const viewsDir = path.join(__dirname, 'views');
-// app.set('views', viewsDir);
-// const staticDir = path.join(__dirname, 'public');
-// app.use(express.static(staticDir));
 app.get('*', (req: Request, res: Response) => {
-	res.send('Oops the resource does not exist');
+    res.status(NOT_FOUND).send('Oops the resource does not exist');
 });
 
+const newsfeed = new Newsfeed(io);
+
+
+
 // Export express instance
-export default app;
+export { server, io};
