8d498fafef917e5651ecefba8501438423c192ca
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-19 17:01:41 +0100

    chore: file clean up and added connect user route

--- src/routes/users/Users.route.ts
+++ src/routes/users/Users.route.ts
@@ -1,11 +1,11 @@
 import { logger } from '../../shared/Logger';
-import {Response, Router, Request } from 'express';
+import { Response, Router, Request } from 'express';
 import { CREATED, OK, INTERNAL_SERVER_ERROR, BAD_REQUEST } from 'http-status-codes';
 import validation from '../../middleware/auth';
 import { User } from '../../entities/User';
 import { IUser } from '../../interfaces/IUser';
 import multer from 'multer';
-import {Utility} from '../../lib/utility';
+import { Utility } from '../../lib/utility';
 
 // Init router and path
 const router = Router();
@@ -37,7 +37,7 @@ router.post(createUserPath, async (req: Request, res: Response) => {
         };
         const result = await User.CreateUser(user);
         if (result.exist) {
-            res.status(BAD_REQUEST).json({exist: true});
+            res.status(BAD_REQUEST).json({ exist: true });
         } else {
             res.status(CREATED).json({
                 message: 'User created',
@@ -68,7 +68,7 @@ router.post(loginPath, async (req: Request, res: Response) => {
                 token: result.token,
             });
         } else {
-            res.status(BAD_REQUEST).json({exist: false});
+            res.status(BAD_REQUEST).json({ exist: false });
         }
     } catch (error) {
         logger.error(error, error.message);
@@ -87,7 +87,7 @@ export const followErrorMessage = 'Oops, something went wrong';
 
 router.post(followUser, auth, async (req: Request, res: Response) => {
     try {
-        const result = await User.FollowUser(req.body.targetUserID, req.body.password);
+        const result = await User.FollowUser(req.body.targetUserID, req.token.userID);
         result === 0 ? res.status(OK).send() : res.status(BAD_REQUEST).send();
     } catch (e) {
         Utility.ErrResponse(res);
@@ -99,7 +99,7 @@ router.post(followUser, auth, async (req: Request, res: Response) => {
  *                   Generic get route for getting user related data
  ******************************************************************************/
 
-export const getUserInfo = '/getUser/:searchKey';  /** Accepted info search Keys: followers, followings, */
+export const getUserInfo = '/getUser/:searchKey';  /** Accepted info search Keys: followers, followings, user*/
 export const getUserInfoErrMessage = 'Oops sorry couldn/t get what you want';
 router.get(getUserInfo, auth, async (req: Request, res: Response) => {
     try {
@@ -119,83 +119,27 @@ const updateUserPath = '/update';
 const errMessage = 'Oops could not update';
 router.post(updateUserPath, auth, async (req: Request, res: Response) => {
     try {
-        const result = await User.UpdateUser(req.body.field, req.token.userID, req.body.update);
-        result === 0 ? res.status(OK).json({msg: 'Updated'}) : res.status(BAD_REQUEST).json(errMessage);
+        const field = req.body.field.toLowerCase();
+        const result = await User.UpdateUser(field, req.token.userID, req.body.update);
+        result === 0 ? res.status(OK).json({ msg: 'Updated' }) : res.status(BAD_REQUEST).json(errMessage);
     } catch (error) {
         Utility.ErrResponse(res);
     }
 });
 
 /******************************************************************************
- *                                  Get Campuses
- ******************************************************************************/
-// export const getCampusesPath = '/getcampuses';
-// router.get(getCampusesPath, async (req: Request, res: Response) => {
-//     try {
-//         const campuses = await GetCampuses(req, res);
-//         res.status(OK).json({
-//             campuses,
-//         });
-//     } catch (error) {
-//         res.status(INTERNAL_SERVER_ERROR).json({
-//             error: 'Oops an error occured',
-//         });
-//     }
-// });
-
-/******************************************************************************
  *                     Get Users From Same And Different Campuses
  /******************************************************************************/
-export const explorePath = '/explore';
-// router.get(explorePath, auth, async (req: Request, res: Response) => {
-//     try {
-//         const onCampus = [];
-//         const otherCampuses = [];
+export const connectPath = '/connect';
+router.get(connectPath, auth, async (req: Request, res: Response) => {
+    try {
+     const result = User.ConnectUser(req.token.userID);
+     res.status(OK).json({result});
+    } catch (error) {
+        Utility.ErrResponse(error);
+    }
+});
 
-//         // On campus
-//         const sameCampus = await UserModel.find({
-//             'userProfile.university': req.token.userID.,
-//         },
-//             {
-//                 followings: 1,
-//                 followers: 1,
-//                 name: 1,
-//                 userProfile: 1,
-//                 userTag: 1,
-//             }).populate([{ path: 'followings' }, { path: 'followers' }]).exec();
-
-//         for (const user of sameCampus) {
-//             // tslint:disable-next-line: max-line-length
-//             onCampus.push({ user, isFollowed: user.checkIsFollowed(req.token.userID), isFollowing: user.checkIsFollowing(req.token.userID) });
-//         }
-
-//         // Other campuses
-//         const diffCampuses = await UserModel.find({
-//             'userProfile.university': { $ne: req.token.userID.university },
-//         }, {
-//             followings: 1,
-//             followers: 1,
-//             name: 1,
-//             userProfile: 1,
-//             userTag: 1,
-//         }).populate([{ path: 'followings' }, { path: 'followers' }]).exec();
-
-//         for (const user of diffCampuses) {
-//             // tslint:disable-next-line: max-line-length
-//             otherCampuses.push({ user, isFollowed: user.checkIsFollowed(req.token.userID), isFollowing: user.checkIsFollowing(req.token.userID) });
-//         }
-
-//         res.status(OK).send({
-//             onCampus,
-//             otherCampuses,
-//         });
-//     } catch (error) {
-//         logger.error(error);
-//         res.status(INTERNAL_SERVER_ERROR).send({
-//             error: 'Oops an error just occurred',
-//         });
-//     }
-// });
 /******************************************************************************
  *                                 Upload Avatar
  /******************************************************************************/
@@ -203,7 +147,7 @@ export const uploadAvatarPath = '/avatar/upload';
 router.post(uploadAvatarPath, auth, upload.single('image'), async (req: Request, res: Response) => {
     try {
         const result = await User.UploadAvatar(req.file, req.token.userID);
-        res.status(OK).json({result});
+        res.status(OK).json({ result });
     } catch (error) {
         Utility.ErrResponse(res);
     }
@@ -216,7 +160,7 @@ export const availableUserTag = '/userTag/:tag';
 router.get(availableUserTag, async (req: Request, res: Response) => {
     try {
         const userTag = await User.AvailableUserTag(req.params.tag);
-        userTag === 0 ? res.status(OK).json({available: true}) : res.status(OK).json({available: false});
+        userTag === 0 ? res.status(OK).json({ available: true }) : res.status(OK).json({ available: false });
     } catch (e) {
         Utility.ErrResponse(res);
     }
@@ -225,20 +169,7 @@ router.get(availableUserTag, async (req: Request, res: Response) => {
 /******************************************************************************
  *                          Get Follower Notification
  /******************************************************************************/
-// export const followingNotificationPath = '/notification/follower';
-// router.get(followingNotificationPath, auth, async (req: Request, res: Response) => {
-//     try {
-//         const followers = await FollowsModel.find({ target: req.token.userID
-//             .populate('follower', { 'name': 1, 'userTag': 1, 'userProfile.avatar': 1 }).exec();
-//         await followers.reverse();
-//         res.status(OK).json({
-//             followers,
-//         }); ,,
-//     };  catch (error) {
-//         logger.error(error);
-//         res.status(INTERNAL_SERVER_ERROR).json({
-//             error: 'Oops an error just occured',
-//         });,
-//   );
+export const followingNotificationPath = '/notification/follower';
+
 
 export default { router, path };
