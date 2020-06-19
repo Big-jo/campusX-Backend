9b69a8e555c2b2997be929297c357a27adf0e0a2
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-19 16:59:08 +0100

    chore: Added campus router

--- src/routes/Base.ts
+++ src/routes/Base.ts
@@ -3,6 +3,7 @@ import UserRouter from './users/Users.route';
 import PostRouter from './posts/Post.route';
 import StoreRouter from './stores/Store.route';
 import CirclesRouter from './circles/Circles.route';
+import campusRouter from './campus/campus.route';
 
 // Init router and path
 const router = Router();
@@ -13,5 +14,7 @@ router.use(UserRouter.path, UserRouter.router);
 router.use(PostRouter.path, PostRouter.router);
 router.use(StoreRouter.path, StoreRouter.router);
 router.use(CirclesRouter.path, CirclesRouter.router);
+router.use(campusRouter.path, campusRouter.router);
+
 // Export the base-router
 export default { router, path };
