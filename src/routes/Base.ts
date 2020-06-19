c5909537cd1c1f8cb615da5e1469e6069b01be8b
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-19 20:31:54 +0100

    chore: Add merchant route

--- src/routes/Base.ts
+++ src/routes/Base.ts
@@ -4,6 +4,7 @@ import PostRouter from './posts/Post.route';
 import StoreRouter from './stores/Store.route';
 import CirclesRouter from './circles/Circles.route';
 import campusRouter from './campus/campus.route';
+import merchantRouter from './merchant/Merchant.route';
 
 // Init router and path
 const router = Router();
@@ -15,6 +16,6 @@ router.use(PostRouter.path, PostRouter.router);
 router.use(StoreRouter.path, StoreRouter.router);
 router.use(CirclesRouter.path, CirclesRouter.router);
 router.use(campusRouter.path, campusRouter.router);
-
+router.use(merchantRouter.path, merchantRouter.router);
 // Export the base-router
 export default { router, path };
