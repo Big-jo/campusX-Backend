f9844f7a38c95b4a54fd67d14690516e489668d2
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-19 17:03:32 +0100

    chore: minor changes

--- src/spec/entities-spec/Post.test.ts
+++ src/spec/entities-spec/Post.test.ts
@@ -9,7 +9,7 @@ import UserModel from 'src/models/User.model';
 import followers from 'src/models/Follower.model';
 import following from 'src/models/Following.model';
 import { Post } from '../../entities/Post';
-import { IPost } from '../../interfaces/IPost';
+import {IComment, IPost} from '../../interfaces/IPost';
 import mongoose from 'mongoose';
 import IORedis from 'ioredis';
 // tslint:disable-next-line:import-spacing
@@ -18,6 +18,8 @@ import { error } from 'winston';
 
 const should = chai.should();
 const userID = '5dda8548843d9d433ed23b4e';
+const userID2 = '5dda87f687ef1e4394fce929';
+const postID = '5e91ea9a2817fa581481411f';
 const redisPort = Number(process.env.REDIS_PORT);
 const client = new IORedis(redisPort);
 
@@ -39,28 +41,29 @@ before(() => {
         // console.log(x);
     });
 
-    client.on('error', (err) => {
+    client.on('error', err => {
         // console.error(err);
         console.log(err);
     });
 });
 
 describe('Post Related Functions', () => {
-    it('should create a new post',done => {
+    it('should create a new post', done => {
         const post: IPost = {
             text: 'Testing 5',
             author: '5dda8548843d9d433ed23b4e',
             userTag: 'Big-Jo',
+            campus: 'Bells University Of Technology',
         };
         Post.CreatePost(post, userID, client, {anonymous: false}).then(value => {
-            expect(value).to.be.a('number');
+            expect(value!.opsValue).to.be.a('number');
             done();
         }).catch(reason => {
             console.log(reason);
         });
     });
 
-    it('should like a post', (done) => {
+    it('should like a post', done => {
         Post.LikePost(userID, '5e91ea9a2817fa581481411f').then(value => {
             expect(value).to.be.a('number');
             done();
@@ -68,30 +71,49 @@ describe('Post Related Functions', () => {
     });
 
     // TODO: A user shouldn't be able to both like and unlike a post
-    it('should dislike a post', (done) => {
-        Post.DislikePost(userID, '5e91eb8dc030c258ea58247b').then((value) => {
+    it('should dislike a post', done => {
+        Post.DislikePost(userID, '5e91eb8dc030c258ea58247b').then(value => {
             expect(value).to.be.a('number');
             done();
         }).catch(done);
     });
 
     // TODO: A user shouldn't be able to both like and unlike a post
-    it('should trash a post', (done) => {
-        Post.DislikePost(userID, '5dda87f687ef1e4394fce929').then((value) => {
-            expect(value).to.be.a('number');
-            done();
-        }).catch(done);
-    });
+    // it('should trash a post', (done) => {
+    //     Post.DislikePost(userID, '5dda87f687ef1e4394fce929').then((value) => {
+    //         expect(value).to.be.a('number');
+    //         done();
+    //     }).catch(done);
+    // });
 
-    it('should get newsfeed', (done) => {
+    it('should get newsfeed', done => {
         // tslint:disable-next-line:max-line-length
-        Post.GetPosts(client, '5dda87f687ef1e4394fce929', { mostRecent: true }).then(reply => {
+        Post.GetPosts(client, userID2, { mostRecent: true }).then(reply => {
             expect(reply).to.have.property('newsfeed');
-            expect(reply!.newsfeed).to.be.an('Array');
+            expect(reply!.newsfeed).to.be.an('Object');
             done();
         }).catch(done);
     });
 
+    it('should comment on a post', () => {
+        const comment: IComment = {
+            campus: 'Bells University Of Technology',
+            name: 'Ricko Lime',
+            userTag: '@Rickyy',
+            author: userID,
+            text: 'Oh well !!',
+            parentPost: postID,
+        };
+        Post.Comment(comment).then(value => {
+            expect(value).to.be.a('number');
+        });
+    });
+
+    it('should get all comments on a post', () => {
+        Post.GetComments(postID).then(value => {
+            expect(value.comments).to.be.an('object');
+        });
+    });
     // it('should get home timeline', function () {
     //
     // });
