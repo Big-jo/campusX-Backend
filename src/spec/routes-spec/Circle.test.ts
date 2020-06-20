98da2200014aa16aab6a7de13fce6249381ecc64
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-20 13:59:14 +0100

    chore: minor changes

--- src/spec/routes-spec/Circle.test.ts
+++ src/spec/routes-spec/Circle.test.ts
@@ -19,90 +19,90 @@ let agent: SuperTest<Test>;
 agent = supertest.agent(app);
 
 before(done => {
-	const URI = process.env.MONGO_URI as string;
-	mongoose.connect(URI, {
-		useNewUrlParser: true,
-		useFindAndModify: false,
-		useCreateIndex: true,
-	});
-	const Db = mongoose.connection;
-	// tslint:disable-next-line: no-console
-	Db.on('error', console.error.bind(console, 'MongoDB connection error'));
-	// tslint:disable-next-line: no-console
-	Db.on('connected', console.log.bind(console, 'MongoDB connected'));
-	done();
+    const URI = process.env.MONGO_URI as string;
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    });
+    const Db = mongoose.connection;
+    // tslint:disable-next-line: no-console
+    Db.on('error', console.error.bind(console, 'MongoDB connection error'));
+    // tslint:disable-next-line: no-console
+    Db.on('connected', console.log.bind(console, 'MongoDB connected'));
+    done();
 });
 
 after(done => {
-	mongoose.disconnect();
-	done();
+    mongoose.disconnect();
+    done();
 });
 
 describe('create circle', () => {
-	const circleObject: ICircle = {
-		avatar: 'None',
-		description: 'Just a space for Intellectual people',
-		name: 'Intellectuals',
-	};
+    const circleObject: ICircle = {
+        avatar: 'None',
+        description: 'Just a space for Intellectual people',
+        name: 'Intellectuals',
+    };
 
-	it('should create a new circle', done => {
-		agent.post(`${BaseApi}/create`).send({circleObject}).end((err, res) => {
-			expect(res.status).to.equal(CREATED);
-			done();
-		});
-	});
+    it('should create a new circle', done => {
+        agent.post(`${BaseApi}/create`).send({circleObject}).end((err, res) => {
+            expect(res.status).to.equal(CREATED);
+            done();
+        });
+    });
 });
 
 describe('join circle', () => {
-	it('should join a circle return a memberID and 200', done => {
-		agent.post(`${BaseApi}/join`).end((err, res) => {
-			expect(res.status).to.equal(OK);
-			expect(res.body.memberID).to.be.a('string');
-			done();
-		});
-	});
+    it('should join a circle return a memberID and 200', done => {
+        agent.post(`${BaseApi}/join`).end((err, res) => {
+            expect(res.status).to.equal(OK);
+            expect(res.body.memberID).to.be.a('string');
+            done();
+        });
+    });
 });
 
 describe('leave circle', () => {
-	it('should join a circle and return 200', done => {
-		const object = {
-			userID,
-			circleID,
-		};
-		agent.post(`${BaseApi}/join`).send(object).end((err, res) => {
-			logger.error(err);
-			expect(res.status).to.equal(OK);
-			done();
-		});
-	});
+    it('should join a circle and return 200', done => {
+        const object = {
+            userID,
+            circleID,
+        };
+        agent.post(`${BaseApi}/join`).send(object).end((err, res) => {
+            logger.error(err);
+            expect(res.status).to.equal(OK);
+            done();
+        });
+    });
 });
 
 describe('get circle feed', () => {
-	it('should return a feed array with posts', done => {
-		agent.get(`${BaseApi}/circle-feed`).end((err, res) => {
-			expect(res.status).to.equal(OK);
-			expect(res.body).to.have.property('circleFeed');
-			done();
-		});
-	});
+    it('should return a feed array with posts', done => {
+        agent.get(`${BaseApi}/circle-feed`).end((err, res) => {
+            expect(res.status).to.equal(OK);
+            expect(res.body).to.have.property('circleFeed');
+            done();
+        });
+    });
 });
 
 describe('create a circle post', () => {
-	const object = {
-		post: {
-			userTag: '@Big-jo',
-			author: 'Joseph Henshaw',
-			text: 'Test 2',
-		},
-		memberID: '5ea197f080c21b4ead7a254a',
-		circleID: '5ea194efaf495f482aec212c',
-	};
+    const object = {
+        post: {
+            userTag: '@Big-jo',
+            author: 'Joseph Henshaw',
+            text: 'Test 2',
+        },
+        memberID: '5ea197f080c21b4ead7a254a',
+        circleID: '5ea194efaf495f482aec212c',
+    };
 
-	it('should create a circle post and return 201', done => {
-		agent.post(`${BaseApi}/post`).send(object).end((err, res) => {
-			logger.error(err);
-			expect(res.status).to.equal(CREATED);
-			done();
-		});
-	});
+    it('should create a circle post and return 201', done => {
+        agent.post(`${BaseApi}/post`).send(object).end((err, res) => {
+            logger.error(err);
+            expect(res.status).to.equal(CREATED);
+            done();
+        });
+    });
 });
