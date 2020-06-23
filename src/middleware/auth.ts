11175914c1175e2cfd9ab1f036c457a1f3ad6ba8
Joseph Henshaw <furiousjoe16@gmail.com>
2020-06-23 22:49:48 +0100

    chore: Remove userProfile

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,33 +6,32 @@ import { IUser, IUserProfile } from 'src/interfaces/IUser';
 import { logger } from '@shared';
 
 const validation = {
-	validateToken: (req: any, res: Response, next: NextFunction) => {
-		const authorizationHeader = req.headers.authorization;
-		let result: object | string;
+    validateToken: (req: any, res: Response, next: NextFunction) => {
+        const authorizationHeader = req.headers.authorization;
+        let result: object | string;
 
-		if (authorizationHeader) {
-			const token = authorizationHeader.split(' ')[1];
-			try {
-				// Verify the token
-				const secret = process.env.JWT_SECRET as string;
-				result = jwt.verify(token, secret);
-				req.token = result as {
-					userID: string,
-					userProfile: IUserProfile,
-				};
+        if (authorizationHeader) {
+            const token = authorizationHeader.split(' ')[1];
+            try {
+                // Verify the token
+                const secret = process.env.JWT_SECRET as string;
+                result = jwt.verify(token, secret);
+                req.token = result as {
+                    userID: string,
+                };
 
-				next();
-			} catch (error) {
-				logger.error(error.error);
-				// Throw an error if anything goes wrong with verification
-				throw new Error(error);
-			}
-		} else {
-			res.status(UNAUTHORIZED).json({
-				error: 'Sorry you\'re not authorized to use this endpooint',
-			});
-		}
-	},
+                next();
+            } catch (error) {
+                logger.error(error.error);
+                // Throw an error if anything goes wrong with verification
+                throw new Error(error);
+            }
+        } else {
+            res.status(UNAUTHORIZED).json({
+                error: 'Sorry you\'re not authorized to use this endpooint',
+            });
+        }
+    },
 };
 
 export default validation;
