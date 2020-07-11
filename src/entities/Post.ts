cbbc030e1e3cf7791ff17d66c60e3669d691876e
Joseph Henshaw <furiousjoe16@gmail.com>
2020-07-11 16:23:41 +0100

    chore: Add support for uploading images

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,13 +1,14 @@
 import PostModel from '../models/Post.model';
-import FollowingModel, {IFollowing} from '../models/Following.model';
+import FollowingModel, { IFollowing } from '../models/Following.model';
 import UserModel from '../models/User.model';
-import {IComment, IPost, IPostModel} from '../interfaces/IPost';
-import {logger} from '../shared/Logger';
+import { IComment, IPost, IPostModel } from '../interfaces/IPost';
+import { logger } from '../shared/Logger';
 import arraySort from 'array-sort';
 import moment from 'moment';
-import FollowerModel, {IFollower} from '../models/Follower.model';
+import FollowerModel, { IFollower } from '../models/Follower.model';
 import * as IORedis from 'ioredis';
 import CommentModel from '../models/Comment.model';
+import { S3 } from '../lib/s3';
 
 // import { bool } from 'aws-sdk/clients/signer';
 
@@ -21,96 +22,107 @@ export interface IPostOptions {
 }
 
 export class Post {
-    constructor() {  }
+    constructor() { }
 
     // tslint:disable-next-line: max-line-length
     public static async CreatePost(postObject: IPost, userID: string, client: IORedis.Redis, options: IPostOptions) {
         try {
-            
+
             // TODO: Optimize this block
-            if (options.anonymous) {
-                let post = await new PostModel({
-                    text: postObject.text,
-                    video: postObject.video,
-                    image: postObject.image,
-                    createdAt: moment().format('lll'),
-                    campus: postObject.campus,
-                });
+            // if (options.anonymous) {
+            //     let post = await new PostModel({
+            //         text: postObject.text,
+            //         video: postObject.video,
+            //         image: postObject.image,
+            //         createdAt: moment().format('lll'),
+            //         campus: postObject.campus,
+            //     });
 
-                post = await post.save();
+            //     post = await post.save();
 
-                const followers: IFollower[] = await FollowerModel.find({target: userID});
-                const updatedFeeds: Array<{ updatedHash: string, newPostID: string }> = [];
+            //     const followers: IFollower[] = await FollowerModel.find({target: userID});
+            //     const updatedFeeds: Array<{ updatedHash: string, newPostID: string }> = [];
 
-                // Add post to campus Feed
-                await client.hmset(post.campus, {[post._id]: post, state: 'dirty'});
+            //     // Add post to campus Feed
+            //     await client.hmset(post.campus, {[post._id]: post, state: 'dirty'});
 
-                // Check if a campus is a member of the set, if not, add them
+            //     // Check if a campus is a member of the set, if not, add them
 
-                // TODO: Stop doing this on each post
-                if ( await client.sismember('campuses', post.campus) === 0) {
-                    await client.sadd('campuses', post.campus);
-                }
+            //     // TODO: Stop doing this on each post
+            //     if ( await client.sismember('campuses', post.campus) === 0) {
+            //         await client.sadd('campuses', post.campus);
+            //     }
 
-                for (const follower of followers) {
+            //     for (const follower of followers) {
 
-                    /**
-                     * Set the state of a users newsfeed in the cache
-                     * - sanitized: It hasnt been updated
-                     * - dirty: It has been updated
-                     */
-                    await client.hmset(follower.follower, {[post._id]: post, state: 'dirty'});
-                    updatedFeeds.push({updatedHash: follower.follower, newPostID: post._id});
-                }
+            //         /**
+            //          * Set the state of a users newsfeed in the cache
+            //          * - sanitized: It hasnt been updated
+            //          * - dirty: It has been updated
+            //          */
+            //         await client.hmset(follower.follower, {[post._id]: post, state: 'dirty'});
+            //         updatedFeeds.push({updatedHash: follower.follower, newPostID: post._id});
+            //     }
 
-                // Also return ID of the newsfeed updated
-                return {
-                    opsValue: 0,
-                    updatedFeeds,
-                };
+            //     // Also return ID of the newsfeed updated
+            //     return {
+            //         opsValue: 0,
+            //         updatedFeeds,
+            //     };
 
-            } else {
-                let post = await new PostModel({
-                    author: userID,
-                    userTag: postObject.userTag,
-                    text: postObject.text,
-                    video: postObject.video,
-                    image: postObject.image,
-                    createdAt: moment().format('lll'),
-                    name: postObject.name,
-                    campus: postObject.campus,
-                });
+            // } else {
 
-                post = await post.save();
+            let post = await new PostModel({
+                author: userID,
+                userTag: postObject.userTag,
+                text: postObject.text,
+                video: '',
+                image: '',
+                createdAt: moment().format('lll'),
+                name: postObject.name,
+                campus: postObject.campus,
+            });
 
-                const followers: IFollower[] = await FollowerModel.find({target: userID});
-                const updatedFeeds: Array<{ updatedHash: string, newPostID: string }> = [];
+            if (postObject.image != null) {
+                const s3 = new S3(post._id + 'image', postObject.image, 'image');
+                post.image = await s3.UploadImage() as string;
+            } 
 
-                // Add post to campus Feed
-                await client.hmset(post.campus, {[post._id]: post, state: 'dirty'});
-
-                // TODO: Stop doing this on each post
-                if ( (await client.sismember('campuses', post.campus)) === 0) {
-                    await client.sadd('campuses', post.campus);
-                }
-
-                for (const follower of followers) {
-
-                    /**
-                     * Set the state of a users newsfeed in the cache
-                     * - sanitized: It hasnt been updated
-                     * - dirty: It has been updated
-                     */
-                    await client.hmset(follower.follower, {[post._id]: post, state: 'dirty'});
-                    updatedFeeds.push({updatedHash: follower.follower, newPostID: post._id});
-                }
-
-                // Also return ID of the newsfeed updated
-                return {
-                    opsValue: 0,
-                    updatedFeeds,
-                };
+            if (postObject.video) {
+                const s3 = new S3(post._id + 'video', postObject.video, 'video');
+                post.video = await s3.UploadVideo() as string;
             }
+            
+            post = await post.save();
+
+            const followers: IFollower[] = await FollowerModel.find({ target: userID });
+            const updatedFeeds: Array<{ updatedHash: string, newPostID: string }> = [];
+
+            // Add post to campus Feed
+            await client.hmset(post.campus, { [post._id]: post, state: 'dirty' });
+
+            // TODO: Stop doing this on each post
+            if ((await client.sismember('campuses', post.campus)) === 0) {
+                await client.sadd('campuses', post.campus);
+            }
+
+            for (const follower of followers) {
+
+                /**
+                 * Set the state of a users newsfeed in the cache
+                 * - sanitized: It hasnt been updated
+                 * - dirty: It has been updated
+                 */
+                await client.hmset(follower.follower, { [post._id]: post, state: 'dirty' });
+                updatedFeeds.push({ updatedHash: follower.follower, newPostID: post._id });
+            }
+
+            // Also return ID of the newsfeed updated
+            return {
+                opsValue: 0,
+                updatedFeeds,
+            };
+            // }
         } catch (error) {
             logger.error(error);
         }
@@ -137,11 +149,11 @@ export class Post {
                     //     }
                     // }
 
-                    return {newsfeed: cachedPosts};
+                    return { newsfeed: cachedPosts };
                 } else {
                     const followings = await FollowingModel.find({
-                            follower: userID,
-                        },
+                        follower: userID,
+                    },
                         {
                             target: 1,
                         },
@@ -154,9 +166,9 @@ export class Post {
                         arr.push(x.target);
                     });
 
-                    const Posts = await PostModel.find({author: {$in: arr}});
+                    const Posts = await PostModel.find({ author: { $in: arr } });
                     // const newsfeed = await this.SortPost(Posts, {reverse: true});
-                    return {Posts};
+                    return { Posts };
                 }
             } catch (error) {
                 logger.error(error);
@@ -170,8 +182,8 @@ export class Post {
             /**
              *  Increment Post likes and update the likedBy field
              */
-            await PostModel.findByIdAndUpdate(postID, {$inc: {likes: 1}, likedBy: userID}).exec();
-            await UserModel.findByIdAndUpdate(userID, {$inc: {'userProfile.rep_points': 0.25}}).exec();
+            await PostModel.findByIdAndUpdate(postID, { $inc: { likes: 1 }, likedBy: userID }).exec();
+            await UserModel.findByIdAndUpdate(userID, { $inc: { 'userProfile.rep_points': 0.25 } }).exec();
             return 0;
         } catch (error) {
             logger.error(error);
@@ -181,8 +193,8 @@ export class Post {
 
     public static async DislikePost(userID: string, postID: string) {
         try {
-            PostModel.findByIdAndUpdate(postID, {$inc: {dislikes: 1}}).exec();
-            UserModel.findByIdAndUpdate(userID, {$inc: {'userProfile.rep_points': 0.20}}).exec();
+            PostModel.findByIdAndUpdate(postID, { $inc: { dislikes: 1 } }).exec();
+            UserModel.findByIdAndUpdate(userID, { $inc: { 'userProfile.rep_points': 0.20 } }).exec();
             return 0;
         } catch (error) {
             throw new Error(error);
@@ -201,12 +213,12 @@ export class Post {
 
     public static async GetComments(postID: string) {
         try {
-            const comments = await CommentModel.find({parentPost: postID})
+            const comments = await CommentModel.find({ parentPost: postID })
                 .lean()
-                .populate('author', {name: 1, userProfile: 1, userTag: 1})
+                .populate('author', { name: 1, userProfile: 1, userTag: 1 })
                 .populate('parentPost')
                 .exec();
-            return {comments};
+            return { comments };
         } catch (e) {
             logger.error(e);
             throw new Error(e);
