ca947aec51ff43731870554ae4ca283bcbbaf25e
Joseph Henshaw <furiousjoe16@gmail.com>
2020-07-17 19:20:21 +0100

    chore: remove check function

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -22,37 +22,5 @@ const UserSchema: Schema = new Schema({
     // lastActive: {type: Date},  Implement last active
 });
 
-// Check if this user is followed by the user making the request
-
-UserSchema.methods.checkIsFollowed = function(id: string) {
-    if (check(this.followers, id, 'follower')) {
-        return true;
-    } else {
-        return false;
-     }
-};
-
-// Check if this user is following the user making the request
-UserSchema.methods.checkIsFollowing = function(id: string) {
-    if (check(this.followings, id, 'target')) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-// Check object in an array if a particular value is present
-function check(array: [], value: string, field: string) {
-    for (const obj of array) {
-        let value1 = obj[field] as string;
-        value1 = value1.toString();
-
-        if (value1 === value) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
 // Export the model and return IUser interface
 export default mongoose.model<IUserModel>('User', UserSchema);
