70e5e292a1051294587ef660c2f6bbc795142d4a
Joseph Henshaw <furiousjoe16@gmail.com>
2020-07-27 17:00:49 +0100

    chore:extend model

--- src/interfaces/ICircle.ts
+++ src/interfaces/ICircle.ts
@@ -1,16 +1,15 @@
-import { Document } from 'mongoose';
+import {Document, PaginateModel} from 'mongoose';
 
-export interface ICircle {
+export interface ICircle extends Document {
     name: string;
     description: string;
     avatar: any;
+    moderators: IModerator[];
+    members_count: number;
 }
 
 interface IModerator {
     user: string;
 }
 
-export interface ICircleModel extends Document, ICircle {
-    moderators: IModerator[];
-    members_count: number;
-}
+export interface ICircleModel <T extends Document> extends PaginateModel<T> {}
