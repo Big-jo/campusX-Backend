486339c6fa8e80d07e0a7d1a992482c62acb0513
Joseph Henshaw <furiousjoe16@gmail.com>
2020-07-27 16:34:53 +0100

    chore: change indentation

--- src/models/CirclePost.model.ts
+++ src/models/CirclePost.model.ts
@@ -4,15 +4,15 @@ import { IPostModel } from 'src/interfaces/IPost';
 import { ICirclePostModel } from 'src/interfaces/ICirclePost';
 
 const CirclePostSchema: Schema = new Schema({
-	author: { type: String, ref: 'User' },
-	userTag: { type: String},
-	text: { type: String },
-	video: { type: String, default: null },
-	image: { type: String, default: null },
-	likes: { type: Number, default: 0 },
-	dislikes: { type: Number, default: 0 },
-	trash: { type: Number, default: 0 },
-	createdAt: { type: String, default: Date.now() },
+    author: { type: String, ref: 'User' },
+    userTag: { type: String},
+    text: { type: String },
+    video: { type: String, default: null },
+    image: { type: String, default: null },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    trash: { type: Number, default: 0 },
+    createdAt: { type: String, default: Date.now() },
 });
 
 CirclePostSchema.methods.score = function score() {}
