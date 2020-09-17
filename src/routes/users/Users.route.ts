f68ac35f7ccbab2476c9cd80646c32d7afcdc26e
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-17 13:31:52 +0100

    fix: returning self when seraching for other users

--- src/routes/users/Users.route.ts
+++ src/routes/users/Users.route.ts
@@ -98,7 +98,7 @@ export const getUserInfoErrMessage = 'Oops sorry couldn/t get what you want';
 router.get(getUserInfo, auth, async (req: Request, res: Response) => {
     try {
         const userID = req.token.userID;
-        const result = await User.GetUser(req.params.searchKey, req.params.targetID, userID);
+        const result = await User.GetUser(req.params.searchKey, req.query.targetID, userID);
         res.status(OK).json({
             result,
         });
