0fd6aca8f44ecb71f7c5b9cf724328bd2f9e0c48
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-21 22:53:18 +0100

    chore: add some types

--- src/models/CirclePost.model.ts
+++ src/models/CirclePost.model.ts
@@ -2,19 +2,21 @@
 import mongoose, { Schema, Document, mongo } from 'mongoose';
 import { IPostModel } from 'src/interfaces/IPost';
 import { ICirclePostModel } from 'src/interfaces/ICirclePost';
+import mongoosePaginate from 'mongoose-paginate';
 
 const CirclePostSchema: Schema = new Schema({
     author: { type: String, ref: 'User' },
-    userTag: { type: String},
+    circle: { type: String, ref: 'Circle' },
+    userTag: { type: String },
     text: { type: String },
     video: { type: String, default: null },
     image: { type: String, default: null },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
     trash: { type: Number, default: 0 },
-    createdAt: { type: String, default: Date.now() },
+    createdAt: { type: String, default: Date },
 });
 
-CirclePostSchema.methods.score = function score() {}
+CirclePostSchema.plugin(mongoosePaginate);
 
 export default mongoose.model<ICirclePostModel>('CirclePost', CirclePostSchema);
