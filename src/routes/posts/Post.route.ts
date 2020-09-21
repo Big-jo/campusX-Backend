61207cf393e22819e9ec4b04e0a8328a3d397646
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-21 14:17:37 +0100

    fix: fix post interactions

--- src/routes/posts/Post.route.ts
+++ src/routes/posts/Post.route.ts
@@ -85,41 +85,41 @@ router.post(createPostPath, auth, upload.single('image'), async (req: Request, r
 /*********************************************************
  *                      Create Comments
  *********************************************************/
-// export const createComment = '/comment';
-// router.post(createComment, auth, async (req: Request, res: Response) => {
-//     try {
-//         const commentObject: IComment = {
-//             video: req.body.video,
-//             image: req.body.image,
-//             text: req.body.text,
-//             author: req.body.author,
-//             userTag: req.body.userTag,
-//             parentPost: req.body.parentPost,
-//             campus: req.body.university,
-//         };
-//         const result = await Post.Comment(commentObject, postCache);
-//         result === 0 ? res.status(CREATED).send() : res.status(BAD_REQUEST).send();
-//     } catch (e) {
-//         logger.error(e);
-//         Utility.ErrResponse(res, e);
-//     }
-// });
+export const createComment = '/comment';
+router.post(createComment, auth, async (req: Request, res: Response) => {
+    try {
+        const commentObject: IComment = {
+            video: req.body.video,
+            image: req.body.image,
+            text: req.body.text,
+            author: req.body.author,
+            userTag: req.body.userTag,
+            parentPost: req.body.parentPost,
+            campus: req.body.university,
+        };
+        const result = await Post.Comment(commentObject, postCache);
+        result === 0 ? res.status(CREATED).send() : res.status(BAD_REQUEST).send();
+    } catch (e) {
+        logger.error(e);
+        Utility.ErrResponse(res, e);
+    }
+});
 
 /*********************************************************
  *                      Get Comments
  *********************************************************/
-// export const getCommentsPath = '/comments/:postID';
+export const getCommentsPath = '/comments/:postID';
 
-// router.get(getCommentsPath, auth, async (req: Request, res: Response) => {
-//     try {
-//         // Actually, just return post with comments sub-field
-//         const result = await Post.GetComments(req.params.postID);
-//         res.status(OK).json({ result });
-//         // TODO: Rank comments
-//     } catch (e) {
-//         Utility.ErrResponse(res, e);
-//     }
-// });
+router.get(getCommentsPath, auth, async (req: Request, res: Response) => {
+    try {
+        // Actually, just return post with comments sub-field
+        const result = await Post.GetComments(req.params.postID);
+        res.status(OK).json({ result });
+        // TODO: Rank comments
+    } catch (e) {
+        Utility.ErrResponse(res, e);
+    }
+});
 /*********************************************************
  *                          Get Posts
  *********************************************************/
@@ -162,27 +162,27 @@ export const getCampusPostPath = '/getnewsfeed/:campusID';
 
 export const likePostPath = '/like';
 
-// router.post(likePostPath, auth, async (req: Request, res: Response) => {
-//     try {
-//         const result = await Post.LikePost(req.token.userID, req.body.postID, postCache);
-//         result === 0 ? res.status(200).send() : res.status(BAD_REQUEST).send();
-//     } catch (error) {
-//         Utility.ErrResponse(res, error);
-//     }
-// });
+router.post(likePostPath, auth, async (req: Request, res: Response) => {
+    try {
+        const result = await Post.LikePost(req.token.userID, req.body.postID, postCache);
+        result === 0 ? res.status(200).send() : res.status(BAD_REQUEST).send();
+    } catch (error) {
+        Utility.ErrResponse(res, error);
+    }
+});
 
 /******************************************************************************
  *                                 Dislike Post
  /******************************************************************************/
-// export const dislikePostPath = '/dislike';
-// router.post(dislikePostPath, auth, async (req: Request, res: Response) => {
-//     try {
-//         const result = await Post.DislikePost(req.token.userID, req.body.postID, postCache);
-//         result === 0 ? res.status(OK).send() : res.status(BAD_REQUEST).send();
-//     } catch (error) {
-//         Utility.ErrResponse(res, error);
-//     }
-// });
+export const dislikePostPath = '/dislike';
+router.post(dislikePostPath, auth, async (req: Request, res: Response) => {
+    try {
+        const result = await Post.DislikePost(req.token.userID, req.body.postID, postCache);
+        result === 0 ? res.status(OK).send() : res.status(BAD_REQUEST).send();
+    } catch (error) {
+        Utility.ErrResponse(res, error);
+    }
+});
 
 
 /******************************************************************************
