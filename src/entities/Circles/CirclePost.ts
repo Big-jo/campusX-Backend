bae17c04d5f21fc7d1381427b3cfc5bb228eac69
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-21 22:54:38 +0100

    chore: cache circle posts, set expire and index it

--- src/entities/Circles/CirclePost.ts
+++ src/entities/Circles/CirclePost.ts
@@ -1,36 +1,54 @@
-import {Post} from '../Post';
+import { Post } from '../Post';
 import CircleCirclePostModel from '../../models/CirclePost.model';
-import {ICirclePost} from '../../interfaces/ICirclePost';
-import {logger} from '../../shared/Logger';
+import { ICirclePost } from '../../interfaces/ICirclePost';
+import { logger } from '../../shared/Logger';
 import IORedis from 'ioredis';
 import CirclePostModel from '../../models/CirclePost.model';
-import {S3} from '../../lib/s3';
+import { S3 } from '../../lib/s3';
+
+interface ICPost {
+    text: string;
+    name: string;
+    userTag: string;
+    author: string;
+}
 
 export class CirclePost extends Post {
 
     // constructor() {}
 
     // tslint:disable-next-line: max-line-length
-    public static async CirclePost(circlePost: { text: string, name: string, userTag: string, circleID: string }, media: any, redisClient: IORedis.Redis, circleID: string, memberID: string) {
+    public static async CirclePost(circlePost: ICPost, media: any, redisClient: IORedis.Redis, circleID: string, memberID: string) {
         try {
             const CPost = {
-                circle: circlePost.circleID,
+                circle: circleID,
                 text: circlePost.text,
                 name: circlePost.name,
                 userTag: circlePost.userTag,
                 video: '',
                 image: '',
+                author: circlePost.author,
+                createdAt: Date(),
             };
 
             const post = new CirclePostModel(CPost);
-            const s3 = new S3(post.id, media.file, 'image');
 
-            media.tag === 'image' ? post.image = await s3.UploadImage() as string : post.video = await s3.UploadImage() as string;
+            if (media !== undefined) {
+                const s3 = new S3(post.id, media.file, 'image');
+
+                media.tag === 'image' ? post.image = await s3.UploadImage() as string : post.video = await s3.UploadImage() as string;
+            }
 
             await post.save();
 
-            const serializedPost = JSON.stringify(post);
-            await redisClient.hset(circleID, memberID, serializedPost);
+            const pipeline = redisClient.pipeline();
+
+            pipeline.hmset(`${circleID}:${post.id}`, CPost);
+            pipeline.expire(`${circleID}:${post.id}`, 172800);
+            pipeline.sadd('circlePostIndex', `${circleID}:${post.id}`);
+
+            pipeline.exec();
+
             return 0;
         } catch (error) {
             logger.error(error);
@@ -43,7 +61,7 @@ export class CirclePost extends Post {
             /**
              *  Increment Post likes and update the likedBy field
              */
-            CirclePostModel.findByIdAndUpdate(postID, {$inc: {likes: 1}, likedBy: userID}).exec();
+            CirclePostModel.findByIdAndUpdate(postID, { $inc: { likes: 1 }, likedBy: userID }).exec();
             return 0;
         } catch (error) {
             logger.error(error);
@@ -53,7 +71,7 @@ export class CirclePost extends Post {
 
     public static async DislikePost(postID: string, userID: string) {
         try {
-            CirclePostModel.findByIdAndUpdate(postID, {$inc: {dislikes: 1}}).exec();
+            CirclePostModel.findByIdAndUpdate(postID, { $inc: { dislikes: 1 } }).exec();
             return 0;
         } catch (error) {
             throw new Error(error);
