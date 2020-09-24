1ff6cc45827d7fff1865d12309dab11e7536b8f6
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-25 00:11:07 +0100

    fix: fix avatar not showing in posts, Add some more properties in CPost Object

--- src/entities/Circles/CirclePost.ts
+++ src/entities/Circles/CirclePost.ts
@@ -5,51 +5,56 @@ import { logger } from '../../shared/Logger';
 import IORedis from 'ioredis';
 import CirclePostModel from '../../models/CirclePost.model';
 import { S3 } from '../../lib/s3';
+import CircleMemberModel from 'src/models/CircleMember.model';
 
-interface ICPost {
-    text: string;
-    name: string;
-    userTag: string;
-    author: string;
-}
+// interface ICPost {
+    
+// }
 
 export class CirclePost extends Post {
 
     // constructor() {}
 
     // tslint:disable-next-line: max-line-length
-    public static async CirclePost(circlePost: ICPost, media: any, redisClient: IORedis.Redis, circleID: string, memberID: string) {
+    public static async CirclePost(circlePost: ICirclePost, media: any, redisClient: IORedis.Redis, circleID: string, memberID: string) {
         try {
-            const CPost = {
-                circle: circleID,
-                text: circlePost.text,
-                name: circlePost.name,
-                userTag: circlePost.userTag,
-                video: '',
-                image: '',
-                author: circlePost.author,
-                createdAt: Date(),
-            };
-
-            const post = new CirclePostModel(CPost);
-
-            if (media !== undefined) {
-                const s3 = new S3(post.id, media.file, 'image');
-
-                media.tag === 'image' ? post.image = await s3.UploadImage() as string : post.video = await s3.UploadImage() as string;
+            const isMember = await CircleMemberModel.findById(circlePost.memberID).lean().exec();
+            if (isMember !== null) {
+                const CPost: ICirclePost = {
+                    circleID: circlePost.circleID,
+                    memberID: circlePost.memberID,
+                    text: circlePost.text,
+                    name: circlePost.name,
+                    userTag: circlePost.userTag,
+                    video: '',
+                    image: '',
+                    author: circlePost.author,
+                    authorAvatar: circlePost.authorAvatar,
+                    campus: circlePost.campus,
+                };
+    
+                const post = new CirclePostModel(CPost);
+    
+                if (media !== undefined) {
+                    const s3 = new S3(post.id, media.file, 'image');
+    
+                    media.tag === 'image' ? post.image = await s3.UploadImage() as string : post.video = await s3.UploadImage() as string;
+                }
+    
+                await post.save();
+    
+                const pipeline = redisClient.pipeline();
+    
+                pipeline.hmset(`${circleID}:${post.id}`, CPost);
+                pipeline.expire(`${circleID}:${post.id}`, 172800);
+                pipeline.sadd(`circlePostsIndexes:${circleID}`, `${circleID}:${post.id}`);
+    
+                pipeline.exec();
+    
+                return 0;
+            } else {
+                return {error: 'Sorry cannot post if you are not a member'};
             }
-
-            await post.save();
-
-            const pipeline = redisClient.pipeline();
-
-            pipeline.hmset(`${circleID}:${post.id}`, CPost);
-            pipeline.expire(`${circleID}:${post.id}`, 172800);
-            pipeline.sadd(`circlePostsIndexes:${circleID}`, `${circleID}:${post.id}`);
-
-            pipeline.exec();
-
-            return 0;
         } catch (error) {
             logger.error(error);
             throw new Error(error);
