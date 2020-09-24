2190034c8e05c963541557b0bfe36b038d0f5fe5
Joseph Henshaw <furiousjoe16@gmail.com>
2020-09-25 00:39:26 +0100

    chore: only use sentry in production

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,5 +1,7 @@
 import * as sentry from '@sentry/node';
 
-sentry.init({dsn: 'https://1c1c4292267644e2b1fbed99f9eebfb0@o411372.ingest.sentry.io/5286469'});
+if (process.env.NODE_ENV === 'production') {
+    sentry.init({dsn: 'https://1c1c4292267644e2b1fbed99f9eebfb0@o411372.ingest.sentry.io/5286469'});
+}
 
 export default sentry;
