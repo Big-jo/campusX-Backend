23b47f34403e6a96483c3a9af7a3b1b6f0c8e0c9
Joseph Henshaw <furiousjoe16@gmail.com>
2020-10-08 11:15:23 +0100

    Chore: Add interface

--- src/models/LikedBy.model.ts
+++ src/models/LikedBy.model.ts
@@ -1,8 +1,13 @@
 import mongoose, {Schema} from 'mongoose';
 
+interface ILikedBy {
+    userID: string;
+    postID: string;
+}
+
 const likedBySchema = new Schema({
     userID: {type: Schema.Types.ObjectId, ref: 'user'},
     postID: {type: Schema.Types.ObjectId, ref: 'post'},
 });
 
-export default mongoose.model('likedBy', likedBySchema);
+export default mongoose.model<ILikedBy>('likedBy', likedBySchema);
