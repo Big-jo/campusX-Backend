811f3379d52a34615287a895d5864c06d0fa1ad5
Joseph Henshaw <furiousjoe16@gmail.com>
2020-10-16 01:40:43 +0100

    initial commit

---
+++ src/models/CircleComment.model.ts
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from 'mongoose';
+import {ICommentModel} from 'src/interfaces/IPost';
+
+const commentSchema: Schema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    parentPost: {type: Schema.Types.ObjectId, ref: 'Post'},
+    parentPostID: {type: String},
+    text: { type: String },
+    video: { type: String, default: null },
+    image: { type: String, default: null },
+    createdAt: {type: Number},
+    likedBy: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+});
+
+commentSchema.index('parentPost');
+
+export default mongoose.model<ICommentModel>('CircleComments', commentSchema);
