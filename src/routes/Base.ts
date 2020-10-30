1e6621e444f1c33c2ab1fc8c6eb03e6508a2d0c2
Joseph Henshaw <furiousjoe16@gmail.com>
2020-10-30 14:29:45 +0100

    chore: Add search handler

--- src/routes/Base.ts
+++ src/routes/Base.ts
@@ -5,6 +5,7 @@ import StoreRouter from './stores/Store.route';
 import CirclesRouter from './circles/Circles.route';
 import campusRouter from './campus/campus.route';
 import merchantRouter from './merchant/Merchant.route';
+import searchRouter from './search/search.route';
 
 // Init router and path
 const router = Router();
@@ -13,9 +14,11 @@ const path = '/api/v1';
 // Add sub-routes
 router.use(UserRouter.path, UserRouter.router);
 router.use(PostRouter.path, PostRouter.router);
+router.use(searchRouter.path, searchRouter.router);
 router.use(StoreRouter.path, StoreRouter.router);
 router.use(CirclesRouter.path, CirclesRouter.router);
 router.use(campusRouter.path, campusRouter.router);
 router.use(merchantRouter.path, merchantRouter.router);
+
 // Export the base-router
 export default { router, path };
