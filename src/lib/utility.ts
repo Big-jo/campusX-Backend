53399be0b1a1d74daa16f0e60a16cfaf693ebdae
Joseph Henshaw <furiousjoe16@gmail.com>
2020-10-30 14:20:27 +0100

    chore: Add try and catch

--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -5,17 +5,19 @@ import {logger} from '@shared';
 import sentry from './sentry';
 export class Utility {
 
-    constructor() {}
-
     public static createToken(payload: any) {
         const secret = process.env.JWT_SECRET as string;
         return jwt.sign(payload, secret);
     }
 
     public static ErrResponse(res: Response, err: any) {
-        const errMsg = { error: err.message };
-        logger.error(err.message);
-        sentry.captureException(err);
-        res.status(INTERNAL_SERVER_ERROR).json(errMsg);
+        try {
+            const errMsg = {error: err.message};
+            logger.error(err.message);
+            sentry.captureException(err);
+            res.status(INTERNAL_SERVER_ERROR).json(errMsg);
+        } catch (e) {
+            logger.error(e);
+        }
     }
 }
