f4f687f899d50a1faee583bdfa9ee320b5b9c309
Joseph Henshaw <furiousjoe16@gmail.com>
2021-01-14 18:20:47 +0100

    chore: all aggrefation queries are in one file

---
+++ src/lib/aggregationQueries.ts
@@ -0,0 +1,74 @@
+import { Types } from "mongoose";
+import PostModel from "../models/Post.model";
+
+export class AggregationQueries {
+    
+    /**
+     * 
+     * @param userID 
+     * @param postIDs
+     */
+    public static async NewsfeedPostAggreg(userID: string, postIDs: Types.ObjectId[]) {
+        const aggregate = [
+            {
+                $match: { _id: { $in: postIDs } },
+            },
+            {
+                $addFields: {
+                    isLiked: { $in: [Types.ObjectId(userID), '$likedBy'] },
+                },
+            },
+            {
+                $project: {
+                    likedBy: 0,
+                },
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    let: { authorID: '$author' },
+                    pipeline: [
+                        { $match: { $expr: { $eq: ['$_id', '$$authorID'] } } },
+                        { $project: { password: 0, email: 0 } },
+                    ],
+                    as: 'author',
+                },
+            },
+        ];
+
+        return (await PostModel.aggregate(aggregate).exec());
+    }
+
+    public static async GetUserPostsAggreg(userID: string, options: {page: number, limit: number}) {
+        const aggregate = [
+            {
+                $match: { author: Types.ObjectId(userID) },
+            },
+            {
+                $addFields: {
+                    isLiked: { $in: [userID, '$likedBy'] },
+                },
+            },
+            {
+                $project: {
+                    likedBy: 0,
+                },
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    let: { authorID: '$author' },
+                    pipeline: [
+                        { $match: { $expr: { $eq: ['$_id', '$$authorID']}}},
+                        { $project: { password: 0, email: 0 } },
+                    ],
+                    as: 'author',
+                },
+            },
+        ];
+
+        const agg = PostModel.aggregate(aggregate);
+        //@ts-ignore
+        return (await PostModel.aggregatePaginate(agg, options));
+    }
+}
