dc1e34f6c0e1584048a7c39c4b13e2df18e75ef3
Joseph Henshaw <furiousjoe16@gmail.com>
2021-01-17 22:01:01 +0100

    chore: add few more stuff

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -1,4 +1,7 @@
+import { logger } from '@shared';
 import * as admin from 'firebase-admin';
+import IORedis from 'ioredis';
+import moment from 'moment';
 
 const serviceAccount = require("../../env/service-file.json");
 
@@ -6,12 +9,52 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
+let primaryCache: IORedis.Redis;
+let postCache: IORedis.Redis;
+
+/******************************************************************************
+ *                                 SETUP REDIS
+ /******************************************************************************/
+
+if (process.env.NODE_ENV === 'development') {
+    primaryCache = new IORedis();
+    postCache = new IORedis({ port: 6379 });
+} else {
+    const redisPortPrimary = Number(process.env.REDIS_PORT_PRIMARY);
+    const redisPortPC = Number(process.env.REDIS_PORT_PC);
+
+    primaryCache = new IORedis(redisPortPrimary, process.env.REDIS_HOST_PRIMARY, { password: process.env.REDIS_PASS_PRIMARY });
+    postCache = new IORedis(redisPortPC, process.env.REDIS_HOST_PC, { password: process.env.REDIS_PASS_PC });
+
+}
+
+primaryCache.on('connect', args => {
+    logger.info('Redis Connected');
+});
+
+primaryCache.on('error', err => {
+    logger.error(err);
+});
+
 export class Notification {
     private fcm = admin.messaging();
 
-    constructor(private deviceToken: string, private notificationPayload: admin.messaging.NotificationMessagePayload) {}
+    constructor(private deviceToken: string, private notificationPayload: admin.messaging.NotificationMessagePayload,
+        private userID: string,
+        private avatar: string,
+        private category: string) { // Allowed categories: like, comment, mention
 
-    public SendToDevice() {
+        const notif = {
+            notificationPayload,
+            avatar: this.avatar,
+            createdAt: moment().utc().valueOf(),
+            category: this.category,
+        }
+        const payload = JSON.stringify(notif)
+        primaryCache.zadd(`notifications:${this.userID}`, notif.createdAt.toString(), payload);
+    }
+
+    public SendPushNotification() {
         const payload: admin.messaging.MessagingPayload = {
             notification: {
                 title: this.notificationPayload.title,
@@ -21,52 +64,13 @@ export class Notification {
         this.fcm.sendToDevice(this.deviceToken, payload);
     }
 
+    public static async GetNotifications(userID: string) {
+        if ((await primaryCache.exists(`notifications:${userID}`) === 1)) {
+            const notif = await primaryCache.zrevrange(`notifications:${userID}`, 0, -1)
+            return { notifications: notif }
+        } else {
+            return { new_notifications: false }
+        }
+
+    }
 }
-
-
-
-// let GCM = require('node-gcm');
-
-// export class Notification {
-
-//     private sender: any
-
-//     private registrationTokens: any[] = [];
-
-//     private message: any;
-
-//     constructor(private deviceToken: string, private title: string, private body: string) {
-//         this.sender = new GCM.Sender(process.env.SERVER_KEY);
-
-//         this.registrationTokens.push(deviceToken);
-
-//         this.message = new GCM.Message({
-//             // collapseKey: 'demo',
-//             priority: 'high',
-//             contentAvailable: true,
-//             delayWhileIdle: true,
-//             timeToLive: 3,
-//             restrictedPackageName: "com.insight.campus"
-//             ,
-//             dryRun: true,
-//             data: {
-//                 key1: 'message1',
-//                 key2: 'message2'
-//             },
-//             notification: {
-//                 title: this.title,
-//                 icon: "ic_launcher",
-//                 body: this.body
-//             }
-//         });
-
-//     }
-
-//     Send() {
-//         console.log('sending')
-//         this.sender.sendNoRetry(this.message, this.registrationTokens, (err: any) => {
-//             if (err) console.log(err);
-//         });
-//     }
-// }
-
