72de1923c44065f3efda2123a4356f586dc5820d
Joseph Henshaw <furiousjoe16@gmail.com>
2021-01-20 16:13:31 +0100

    chore: new endpoint to get users posts

--- src/routes/users/Users.route.ts
+++ src/routes/users/Users.route.ts
@@ -191,13 +191,25 @@ router.get(availableUserTag, async (req: Request, res: Response) => {
 const getUserPosts = '/posts';
 router.get(getUserPosts, auth, async (req, res) => {
     try {
-        const userID = req.query.userID === undefined ? req.token.userID : req.query.userID
-        const result = await User.GetUserPosts(userID, req.query.page, req.query.limit);
+        const result = await User.GetUserPosts(req.query.userID, req.query.page, req.query.limit);
         res.status(OK).json({ result });
     } catch (e) {
         Utility.ErrResponse(res, e);
     }
 });
+
+/******************************************************************************
+*                                 GET CLIENTS POSTS
+/******************************************************************************/
+export const getClientPosts = '/posts/me';
+router.get(getClientPosts, auth, async (req: Request, res: Response) => {
+    try {
+        const result = await User.GetUserPosts(req.token.userID, req.query.page, req.query.limit);
+        res.status(OK).json({ result })
+    } catch (error) {
+        Utility.ErrResponse(error, res);
+    }
+})
 /******************************************************************************
  *                          Get Follower Notification
  /******************************************************************************/
