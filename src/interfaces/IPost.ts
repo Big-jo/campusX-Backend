97ac102939e81d085f5c02fd5fbf5a9a492fb827
Josephj Henshaw <furiousjoe16@gmail.com>
2021-01-31 18:33:14 +0100

    chore: modularize

--- src/interfaces/IPost.ts
+++ src/interfaces/IPost.ts
@@ -16,7 +16,7 @@ export interface IPost {
     comments?: number;
     createdAt?: number;
     parentPost: string;
-    hashTags?: string;
+    hashTags?: string[];
 }
 
 export interface ICommentModel extends IPostModel {
@@ -30,7 +30,7 @@ export interface IComment extends IPost {
     // type: string;
     parentPost: string;
     // parentPostID: string;
-    hashTags?: string 
+    hashTags?: string[] 
 
 }
 
---
+++ src/lib/postParser.ts
@@ -0,0 +1,32 @@
+import { IPost } from '@interfaces';
+import { String } from 'aws-sdk/clients/appstream';
+import { logger } from '../shared';
+const extract = require('mention-hashtag');
+
+export class PostParser {
+    constructor(private post: IPost) {
+
+    }
+
+    public async Parse() {
+        try {
+            const mentions = extract(this.post.text, 'all')
+
+            const mentionedUsers: string[] = mentions.mentions;
+            const hashTags: String[] = mentions.hashtags;
+
+            return {
+                mentionedUsers: {
+                    mentionedUsers,
+                    count: mentionedUsers.length
+                },
+                hashTags: {
+                    hashTags,
+                    count: hashTags.length,
+                },
+            }
+        } catch (e) {
+            logger.error(e);
+        }
+    }
+}
