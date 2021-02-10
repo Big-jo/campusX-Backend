5a7b2fa1242cf15700c58e4104a8f11cc92ec239
Josephj Henshaw <furiousjoe16@gmail.com>
2021-02-10 03:18:36 +0100

    chore: minor changes

--- src/routes/notifcations/notifications.route.ts
+++ src/routes/notifcations/notifications.route.ts
@@ -4,8 +4,8 @@ import { CREATED, INTERNAL_SERVER_ERROR, OK, BAD_REQUEST } from 'http-status-cod
 import { logger } from '@shared';
 import validation from '../../middleware/auth';
 import IORedis from 'ioredis';
-import { Utility } from '../../lib/utility';
-import { Notification } from '@lib'
+import {Notification, Utility} from '@lib';
+import NotificationModel from '../../models/Notification.model';
 const router = Router();
 
 const path = '/notify';
@@ -15,21 +15,11 @@ const auth = validation.validateToken;
 export const getNotif = '/retrieve';
 router.get(getNotif, auth, async (req: Request, res: Response) => {
     try {
-        const result = await Notification.GetNotifications(req.token.userID, res.locals.primaryCache);
-        if (result.new_notifications === false) {
-            res.status(OK).json({ result: [] });
-        } else {
-            const r = [];
-            for (let index = 0; index < result.notifications.length; index++) {
-                const element = result.notifications[index];
-                r.push(JSON.parse(element));
-            }
-            res.status(OK).json({ result: r });
-        }
-
-    } catch (error) {
-        Utility.ErrResponse(res, error);
+        const notifications = await Notification.GetNotifications(req.token.userID);
+        res.json({notifications});
+    } catch (e) {
+        Utility.ErrResponse(res, e);
     }
-})
+});
 
 export default { router, path };
