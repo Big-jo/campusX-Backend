7eced0319dfd5b3056d70edfd9df39489a8c74ea
Josephj Henshaw <furiousjoe16@gmail.com>
2021-02-18 20:24:16 +0100

    chore: reverse notifications

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -57,7 +57,7 @@ export class Notification {
     public static async GetNotifications(userID: string) {
         // logger.info(Types.ObjectId(userID));
         const notifications = await NotificationModel.find({userID}).lean().exec();
-        return {payload: notifications};
+        return {payload: notifications.reverse()};
 
         // if ((await primaryCache.exists(`notifications:${userID}`) === 1)) {
         //     const notif = await primaryCache.zrevrange(`notifications:${userID}`, 0, -1);
