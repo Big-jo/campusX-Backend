2a158347a768db2317930b63fde8f4a808788e30
Josephj Henshaw <furiousjoe16@gmail.com>
2021-03-11 17:18:44 +0100

    Chore: Utility method to keep track of structure members for expiration

--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken';
-import { Response } from 'express';
-import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
-import { logger } from '@shared';
+import {Response} from 'express';
+import {INTERNAL_SERVER_ERROR} from 'http-status-codes';
+import {logger} from '@shared';
 import sentry from './sentry';
-import { ITokenPayload } from 'src/interfaces/ITokenPayload';
+import {ITokenPayload} from 'src/interfaces/ITokenPayload';
+import Moment from 'moment';
+import moment from 'moment';
+import IORedis from 'ioredis';
+
 export class Utility {
 
     public static createToken(payload: ITokenPayload) {
@@ -24,5 +28,19 @@ export class Utility {
     
     public static filterRedisPipeline(piplelineResult: Array<[Error | null, any]>) {
         return piplelineResult.map(value => value[1]);
+    }
+
+    /**
+     *
+     * @param key - Key for structure in redis
+     * @param member - Name of field to be cleared in structure
+     * @param expiryTime - Expiry time
+     * @
+     * @param timeUnit
+     * @param redis
+     */
+    public static CacheExpiryTracker(key: string, member: string, expiryTime: number, timeUnit: Moment.unitOfTime.DurationConstructor, redis: IORedis.Redis) {
+        const ttl = moment().utc().add(expiryTime, timeUnit).valueOf().toString();;
+        redis.zadd(key, ttl, member );
     }
 }
