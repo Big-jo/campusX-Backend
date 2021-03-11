294c229840bbf3c06b5879fcb4d9e1fc37631f1c
Josephj Henshaw <furiousjoe16@gmail.com>
2021-03-11 17:19:08 +0100

    Chore; Create task for expiring members for structures

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -9,7 +9,7 @@ import { logger } from '@shared';
 export class Tasks {
     private mongoUri: string;
     private agenda: agenda;
-    private postCache: IORedis.Redis;
+    private redis: IORedis.Redis;
 
     constructor(MongoUri: string) {
         this.mongoUri = MongoUri;
@@ -27,56 +27,82 @@ export class Tasks {
          /******************************************************************************/
 
         if (process.env.NODE_ENV === 'development') {
-            this.postCache = new IORedis({ port: 6379 });
+            this.redis = new IORedis({ port: 6379 });
         } else {
             const redisPortPrimary = Number(process.env.REDIS_PORT_PRIMARY);
             const redisPortPC = Number(process.env.REDIS_PORT_PC);
 
-            this.postCache = new IORedis(redisPortPC, process.env.REDIS_HOST_PC, { password: process.env.REDIS_PASS_PC });
+            this.redis = new IORedis(redisPortPC, process.env.REDIS_HOST_PC, { password: process.env.REDIS_PASS_PC });
         }
 
-        this.postCache.on('connect', args => {
+        this.redis.on('connect', args => {
             logger.info('Redis Connected, Trending');
         });
 
-        this.postCache.on('error', err => {
+        this.redis.on('error', err => {
             logger.error(err);
         });
     }
 
     public TrendTask() {
-        this.agenda.define('Calculate Trending', async job => {
-            try {
-                // const posts = await PostModel.find({ createdAt: { $gte: new Date().getTime() - (2 * 60 * 60 * 1000) } }).exec();
-                const posts = await PostModel.find({}).exec();
-                const trend = new Trend(posts).GenerateTrend();
-
-                // cache results 
-                const pipeline = this.postCache.pipeline();
-
-                Object.keys(trend).forEach(key => {
-                    // console.log(key, trend[key]);
-                    // console.log('key');
-                    const currentTrendElement = trend[key];
-                    const campus = key;
-                    for (let index = 0; index < (currentTrendElement.length > 5 ? 5 : currentTrendElement.length); index++) {
-                        const element = currentTrendElement[index];
-                        pipeline.zadd(`campusesTrends:${campus}`, `${element.count.toString()}`, element.keyword);
-                    }
-                });
-
-                pipeline.exec();
-            } catch (error) {
-                logger.error(error);
-            }
-        });
-
-        this.agenda.on('ready', () => {
-            this.agenda.start();
-            this.agenda.every('30 minutes', 'Calculate Trending');
-        });
+        // this.agenda.define('Calculate Trending', async job => {
+        //     try {
+        //         // const posts = await PostModel.find({ createdAt: { $gte: new Date().getTime() - (2 * 60 * 60 * 1000) } }).exec();
+        //         const posts = await PostModel.find({}).exec();
+        //         const trend = new Trend(posts).GenerateTrend();
+        //
+        //         // cache results
+        //         const pipeline = this.redis.pipeline();
+        //
+        //         Object.keys(trend).forEach(key => {
+        //             // console.log(key, trend[key]);
+        //             // console.log('key');
+        //             const currentTrendElement = trend[key];
+        //             const campus = key;
+        //             for (let index = 0; index < (currentTrendElement.length > 5 ? 5 : currentTrendElement.length); index++) {
+        //                 const element = currentTrendElement[index];
+        //                 pipeline.zadd(`campusesTrends:${campus}`, `${element.count.toString()}`, element.keyword);
+        //             }
+        //         });
+        //
+        //         pipeline.exec();
+        //     } catch (error) {
+        //         logger.error(error);
+        //     }
+        // });
+        //
+        // this.agenda.on('ready', () => {
+        //     this.agenda.start();
+        //     this.agenda.every('30 minutes', 'Calculate Trending');
+        // });
     }
 
+    public CleanUpRedisTask() {
+        this.agenda.define('Clean Campus Timeline', async job => {
+            try {
+                // Get Current time
+                const unixNow = moment().utc().valueOf();
+
+                // Get campus posts to be removed
+                const expiredPosts = await this.redis.zrevrangebyscore('campusFeedExpiry', 0, unixNow);
+                this.redis.zremrangebyscore('campusFeedExpiry', 0, unixNow);
+                // Filter expired posts to get the campus names
+                const filterExpired = expiredPosts.map(post => {
+                    return { campus: post.split(':')[0], member: post };
+                });
+
+                const pipeline = await this.redis.pipeline();
+                filterExpired.forEach(filtered => pipeline.zrem(`campusFeed:${filtered.campus}`, filtered.member));
+
+                this.agenda.on('ready', args => {
+                    this.agenda.start();
+                    this.agenda.every('10 hours', 'Clean Campus Timeline')
+                });
+            } catch (e) {
+                logger.error(e);
+            }
+        });
+    }
     // public GenerateFakePosts() {
     //     for (let index = 0; index < 100000; index++) {
     //         const post = new PostModel({
