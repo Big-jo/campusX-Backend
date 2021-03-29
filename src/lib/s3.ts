c59f55efd632b3d39072b6e6f8602d6d22240070
Josephj Henshaw <furiousjoe16@gmail.com>
2021-03-29 04:17:50 +0200

    chore: change endpoint of object storage

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -7,9 +7,10 @@ import CircleModel from 'src/models/Circle.model';
 export class S3 {
     // public Endpoint = new aws.Endpoint('ams3.digitaloceanspaces.com');
     public s3Bucket = new aws.S3({
-        endpoint: 'ams3.digitaloceanspaces.com',
+        endpoint: 'us-east-1.linodeobjects.com',
         accessKeyId: process.env.SPACES_ACCESS_KEY,
         secretAccessKey: process.env.SPACES_SECRET,
+
     });
     public params: {
         Bucket: string;
