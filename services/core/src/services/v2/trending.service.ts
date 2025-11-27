9d2e811fefa3d8fcd70f3168ecb5815c742c9c53
joseph <furiousjoe16@gmail.com>
2025-11-27 23:52:49 +0100

    fix

--- services/core/src/services/v2/trending.service.ts
+++ services/core/src/services/v2/trending.service.ts
@@ -57,7 +57,7 @@ export class TrendingService {
   constructor() {
     this.postRepo = new PostRepository();
     // Use Redis DB 2 (ML service database)
-    this.redis = RedisClient.getInstance(1);
+    this.redis = RedisClient.getInstance(0);
   }
 
   /**
